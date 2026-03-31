4ebc1a978d2ac915938f7b86c31ee876dcf41090
Arul <arulravichandran95@gmail.com>
2026-03-31 17:18:33 +0530

    Initial commit

---
+++ Leave-Backend/Leave-Backend/src/main/java/com/example/Leave_Backend/LeaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Leave_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveBackendApplication.class, args);
+	}
+
+}
---
+++ Leave-Backend/Leave-Backend/src/test/java/com/example/Leave_Backend/LeaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Leave_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
